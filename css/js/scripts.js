db6aeac22e4a3e4d657622e1ba57a7b4de19f8e2
Leah Neric <nericleah@gmail.com>
2023-12-22 21:20:53 -0500

    Updates

---
+++ css/js/scripts.js
@@ -0,0 +1,30 @@
+
+    $(function() {
+        $(".carousel").carousel( { interval: 2000 } );
+        $("#carouselButton").click(function(){
+            if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+                $(".carousel").carousel("pause");
+                $("#carouselButton").children("i").removeClass("fa-pause");
+                $("#carouselButton").children("i").addClass("fa-play");
+            } else {
+                $(".carousel").carousel("cycle");
+                $("#carouselButton").children("i").removeClass("fa-play");
+                $("#carouselButton").children("i").addClass("fa-pause"); 
+            }
+
+            
+        });
+
+
+        
+    });
+
+   
+$(function() {
+
+    $('#reserveButton').modal('show');
+
+    $('#loginButton').modal('show');
+
+    
+});
